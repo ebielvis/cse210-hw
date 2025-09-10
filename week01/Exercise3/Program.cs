2ea66902899af9f47167417fca4daa544fd33c2b
Ebi-Johnson Elvis <eebijohnson@byupathway.edu>
2025-09-10 09:47:36 +0100

    modify, programs for exercise1, exercise2 and exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("Welcome to the Guess My Number game!");
+            Console.WriteLine("I have picked a number between 1 and 100.");
+            
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out guess))
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    continue;
+                }
+
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {guessCount} guesses.");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string response = Console.ReadLine().Trim().ToLower();
+
+            if (response != "yes")
+            {
+                playAgain = false;
+                Console.WriteLine("Thanks for playing! Goodbye.");
+            }
+        }
     }
 }
